182bf8544cefd0a863457512b8aada952a1d19af
pacdiv <pacdiv@users.noreply.github.com>
2019-07-16 20:14:45 +0200

    Fixed the twitter share link in the bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -11,59 +11,74 @@ import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
 
-const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
+class Bio extends React.PureComponent {
+  state = {
+    twitterUrl: ''
+  }
+
+  componentDidMount() {
+    if (typeof window !== "undefined") {
+      this.setState({
+        twitterUrl:
+          `https://twitter.com/intent/tweet?url=${window.location.href}&via=pacdiv_io`,
+      })
+    }
+  }
+
+  render () {
+    const data = useStaticQuery(graphql`
+      query BioQuery {
+        avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
+          childImageSharp {
+            fixed(width: 50, height: 50) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+        site {
+          siteMetadata {
+            author
           }
         }
       }
-      site {
-        siteMetadata {
-          author
-        }
-      }
-    }
-  `)
+    `)
 
-  const { author } = data.site.siteMetadata
-  return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
+    const { author } = data.site.siteMetadata
+    return (
+      <div
         style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
+          display: `flex`,
+          marginBottom: rhythm(2.5),
         }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Notes from my journey to build an audience as a developer
-        <br />– By <strong>{author}</strong>
-        {` | `}
-        <a
-          className="colored-link"
-          href={window.location}
-          target="blank_"
-        >
-          Let’s talk about this on Twitter
-        </a>
-      </p>
-    </div>
-  )
+      >
+        <Image
+          fixed={data.avatar.childImageSharp.fixed}
+          alt={author}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            minWidth: 50,
+            borderRadius: `100%`,
+          }}
+          imgStyle={{
+            borderRadius: `50%`,
+          }}
+        />
+        <p>
+          Notes from my journey to build an audience as a developer
+          <br />– By <strong>{author}</strong>
+          {` | `}
+          <a
+            className="colored-link"
+            href={this.state.twitterUrl}
+            target="blank_"
+          >
+            Let’s talk about this on Twitter
+          </a>
+        </p>
+      </div>
+    )
+  }
 }
 
 export default Bio
