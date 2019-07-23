f68ab27888a1b1599b952a021d9980c801209f99
pacdiv <25844965+pacdiv@users.noreply.github.com>
2019-07-23 12:33:39 +0200

    Fixed an issue with the back-home link's href (#16)

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ class Layout extends React.Component {
     const rootPath = `${__PATH_PREFIX__}/`
     let header
     const homeLink = location.pathname.startsWith("/blog")
-      ? { path: '/blog', title }
+      ? { path: '/', title }
       : { path: '/blog', title: '← blog' }
 
     if (location.pathname === rootPath) {
